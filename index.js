7d9bc25d22193216383dabff790deb0ffbc9febd
grega <grega.lasnibat@gmail.com>
2020-11-02 16:43:38 +0100

    fetching OMDb data with axios

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hi there!');
+
+const fetchData = async () => {
+  const response = await axios.get(`http://www.omdbapi.com`, {
+    params: {
+      apikey: 45384805,
+      // s: "avengers",
+      i: "tt0848228"
+    },
+  });
+  console.log(response.data);
+};
+
+fetchData();
