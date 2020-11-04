9a2cbd426f15ccbf7e1d6ef5358878bfd697a190
grega <grega.lasnibat@gmail.com>
2020-11-04 22:03:39 +0100

    added movie detail and refactored some code into utils.js

--- index.js
+++ index.js
@@ -1,31 +1,73 @@
+// target the root div and insert html into it
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+<label><b>Search for a movie</b></label>
+<input class="input" />
+<div class="dropdown">
+  <div class="dropdown-menu">
+    <div class="dropdown-content results">
+           
+    </div>
+  </div>
+</div>
+`;
+
+// target various elements
+const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
+
+// function for fetching data from OMDb API
 const fetchData = async (searchTerm) => {
-  const response = await axios.get(`http://www.omdbapi.com`, {
-    params: {
-      apikey: 45384805,
-      s: searchTerm,
-    },
-  });
+  const response = await fetchMovies(searchTerm);
   if (response.data.Error) {
     return [];
   }
   return response.data.Search;
 };
 
-const input = document.querySelector("input");
-
+// gets fired when typing
 const onInput = async (e) => {
   const movies = await fetchData(e.target.value);
 
-  if (movies) {
-    for (let movie of movies) {
-      const div = document.createElement("div");
-      div.innerHTML = `
-      <img src="${movie.Poster}"/>
-      <h1>${movie.Title}</h1>
+  // clear out previous results
+  resultsWrapper.innerHTML = "";
+
+  // close dropdown if not fetching anything (cleared input)
+  if (!movies.length) {
+    dropdown.classList.remove("is-active");
+    return;
+  }
+  // add another class name to dropdown div
+  dropdown.classList.add("is-active");
+  // traverse movies
+  for (let movie of movies) {
+    const option = document.createElement("a");
+    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+    option.classList.add("dropdown-item");
+    option.innerHTML = `
+      <img src="${imgSrc}"/>
+      ${movie.Title}
       `;
-      document.querySelector("#target").appendChild(div);
-    }
+
+    // listen for a click on a single option
+    option.addEventListener("click", () => {
+      // close the dropdown and update input
+      dropdown.classList.remove("is-active");
+      input.value = movie.Title;
+      fetchMovie(movie);
+    });
+
+    resultsWrapper.appendChild(option);
   }
 };
 
+// debounce the onInput call to fire after 1 second of no new input
 input.addEventListener("input", debounce(onInput, 1000));
+
+// listen for a click outside of root container to close dropdown
+document.addEventListener("click", (e) => {
+  if (!root.contains(e.target)) {
+    dropdown.classList.remove("is-active");
+  }
+});
--- utils.js
+++ utils.js
@@ -1,3 +1,65 @@
+// fetchMovies
+const fetchMovies = async (searchTerm) => {
+  const response = await axios.get(`http://www.omdbapi.com`, {
+    params: {
+      apikey: 45384805,
+      s: searchTerm,
+    },
+  });
+  return response;
+};
+
+// onMovieSelect
+const fetchMovie = async (movie) => {
+  const response = await axios.get(`http://www.omdbapi.com`, {
+    params: {
+      apikey: 45384805,
+      i: movie.imdbID,
+    },
+  });
+  document.querySelector("#summary").innerHTML = movieTemplate(response.data);
+};
+
+// movieTemplate
+const movieTemplate = (movieDetail) => {
+  return `
+    <article class="media">
+      <figure class="media-left">
+        <p class="image">
+          <img src="${movieDetail.Poster}" />
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
+        </div>
+      </div>
+    </article>
+    <article class="notification is-primary">
+      <p>${movieDetail.Awards}</p>
+      <p class="subtitle">Awards</p>
+    </article>
+    <article class="notification is-primary">
+      <p>${movieDetail.BoxOffice}</p>
+      <p class="subtitle">Box Office</p>
+    </article>
+    <article class="notification is-primary">
+      <p>${movieDetail.Metascore}</p>
+      <p class="subtitle">Metascore</p>
+    </article>
+    <article class="notification is-primary">
+      <p>${movieDetail.imdbRating}</p>
+      <p class="subtitle">IMDB rating</p>
+    </article>
+    <article class="notification is-primary">
+      <p>${movieDetail.imdbVotes}</p>
+      <p class="subtitle">IMDB Votes</p>
+    </article>
+  `;
+};
+
 // debounce wrapper function for limiting api calls
 const debounce = (func, delay = 1000) => {
   let timeoutId;
