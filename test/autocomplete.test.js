f093a09b0fd7fe73616bb43a1a33ac30b5fba695
grega <grega.lasnibat@gmail.com>
2020-12-02 13:56:06 +0100

    added 3 tests using mocha and chai

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,15 +1,62 @@
-it("Shows an autocomplete", () => {
+// beforeEach hook
+beforeEach(() => {
+  document.querySelector("#target").innerHTML = "";
   createAutoComplete({
     root: document.querySelector("#target"),
     fetchData() {
       return [
-        { Title: "Avengers" },
-        { Title: "Some other movie" },
-        { Title: "Some third movie" },
+        { Title: "First movie" },
+        { Title: "Second movie" },
+        { Title: "Third movie" },
       ];
     },
     renderOption(movie) {
       return movie.Title;
     },
   });
+});
+
+// helper function
+const waitFor = (selector) => {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(() => {
+      if (document.querySelector(selector)) {
+        clearInterval(interval);
+        clearTimeout(timeout);
+        resolve();
+      }
+    }, 30);
+
+    const timeout = setTimeout(() => {
+      clearInterval(interval);
+      reject();
+    }, 3000);
+  });
+};
+
+it("Dropdown starts closed", () => {
+  const dropdown = document.querySelector(".dropdown");
+  expect(dropdown.className).not.to.include("is-active");
+});
+
+it("After searching, dropdown opens", async () => {
+  const input = document.querySelector("input");
+  input.value = "avengers";
+  input.dispatchEvent(new Event("input"));
+
+  await waitFor(".dropdown-item");
+
+  const dropdown = document.querySelector(".dropdown");
+  expect(dropdown.className).to.include("is-active");
+});
+
+it("After searching, display some results", async () => {
+  const input = document.querySelector("input");
+  input.value = "avengers";
+  input.dispatchEvent(new Event("input"));
+
+  await waitFor(".dropdown-item");
+
+  const items = document.querySelectorAll(".dropdown-item");
+  expect(items.length).to.equal(3);
 });
